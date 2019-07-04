e586d95c4a4900b11f82c6ff432471874d8bdcc7
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-07-04 11:29:28 +0200

    Update the tests.

--- spec/drupol/launcher/tests/TestLauncherSpec.php
+++ spec/drupol/launcher/tests/TestLauncherSpec.php
@@ -11,10 +11,34 @@ class TestLauncherSpec extends ObjectBehavior
 {
     public function it_can_launch()
     {
+        switch (PHP_OS_FAMILY) {
+            case 'BSD':
+            case 'Linux':
+            case 'Solaris':
+                $command = 'xdg-open';
+
+                break;
+
+            case 'Darwin':
+                $command = 'open';
+
+                break;
+
+            case 'Windows':
+                $command = 'start';
+
+                break;
+
+            default:
+                throw new \Exception('Unable to find the operating system.');
+
+                break;
+        }
+
         $this::open('https://google.com', 'https://github.com');
 
         $this::getCommands()
-            ->shouldReturn(["'xdg-open' 'https://google.com'", "'xdg-open' 'https://github.com'"]);
+            ->shouldReturn(["{$command} 'https://google.com'", "{$command} 'https://github.com'"]);
     }
 
     public function it_is_initializable()
